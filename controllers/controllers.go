6e533b771c81642345e581834017215ea1a572f9
AmirMohammadKomijani <amir.m.k8011@gmail.com>
2024-08-31 14:25:10 +0330

    <Controller> : Searching Product controller is implemented.

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -150,3 +150,33 @@ func ProductViewerAdmin() gin.HandlerFunc {
 		c.JSON(http.StatusOK, "Successfully added our Product Admin!!")
 	}
 }
+
+func SearchProduct() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var productlist []models.Product
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+		cursor, err := ProductCollection.Find(ctx, bson.D{{}})
+		if err != nil {
+			c.IndentedJSON(http.StatusInternalServerError, "Someting Went Wrong Please Try After Some Time")
+			return
+		}
+		err = cursor.All(ctx, &productlist)
+		if err != nil {
+			log.Println(err)
+			c.AbortWithStatus(http.StatusInternalServerError)
+			return
+		}
+		defer cursor.Close(ctx)
+		if err := cursor.Err(); err != nil {
+			// Don't forget to log errors. I log them really simple here just
+			// to get the point across.
+			log.Println(err)
+			c.IndentedJSON(400, "invalid")
+			return
+		}
+		defer cancel()
+		c.IndentedJSON(200, productlist)
+
+	}
+}
